1315c76d18e721a98ba22e0819a485a3ee36ae53
wardensky <zhaochunhui1985@gmail.com>
2018-08-10 16:56:01 +0800

    modify

--- 7.framework/spring/source-code/spring-code-demo/src/main/java/com/zch/spring_code_demo/demo6/App.java
+++ 7.framework/spring/source-code/spring-code-demo/src/main/java/com/zch/spring_code_demo/demo6/App.java
@@ -6,8 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
 	public static void main(String[] args) {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("demo6.xml");
-		IService2 s2 = (IService2)ac.getBean("service2");
-		 
+		///这个地方如果写成Service2会出问题，“No bean named 'Service2' is defined”
+		IService2 s2 = (IService2) ac.getBean("Service2");
+
 		s2.sing();
 
 		((ClassPathXmlApplicationContext) ac).close();
