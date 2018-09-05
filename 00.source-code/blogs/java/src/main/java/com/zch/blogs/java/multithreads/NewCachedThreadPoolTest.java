6c486d7d0956512280a3f26e73e30c3b736cbde6
wardensky <zhaochunhui1985@gmail.com>
2018-09-05 19:09:10 +0800

    a

---
+++ 00.source-code/blogs/java/src/main/java/com/zch/blogs/java/multithreads/NewCachedThreadPoolTest.java
@@ -0,0 +1,26 @@
+package com.zch.blogs.java.multithreads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class NewCachedThreadPoolTest {
+
+	public static void main(String[] args) {
+		int sleepTime = 1;
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		for (int i = 0; i < 5; i++) {
+			executorService.execute(new Runnable() {
+				public void run() {
+					try {
+						Thread.sleep(1000 * sleepTime );
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println(Thread.currentThread().getName() + "线程被调用了。");
+				}
+			});
+			System.out.println("************* a" + i + " *************");
+		}
+		executorService.shutdown();
+	}
+}
