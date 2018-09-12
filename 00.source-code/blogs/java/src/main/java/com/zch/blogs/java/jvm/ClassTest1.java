97362c89b67448c96659035cf730e1e587309e5d
wardensky <zhaochunhui1985@gmail.com>
2018-09-12 10:54:50 +0800

    a

---
+++ 00.source-code/blogs/java/src/main/java/com/zch/blogs/java/jvm/ClassTest1.java
@@ -0,0 +1,14 @@
+package com.zch.blogs.java.jvm;
+
+public class ClassTest1 {
+	private static int i;
+	private double d;
+
+	public static void print() {
+
+	}
+
+	private boolean trueOrFalse() {
+		return false;
+	}
+}
