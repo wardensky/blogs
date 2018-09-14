3d1f048c46e400aef5270f73ff889a770e3cf026
wardensky <zhaochunhui1985@gmail.com>
2018-09-14 19:08:58 +0800

    a

--- 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/MergeSortDemo.java
+++ 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/MergeSortDemo.java
@@ -1,24 +1,66 @@
 package com.zch.blogs.algorithm.sort;
 
+/**
+ * @Description 归并的含义是将两个或两个以上的有序表合并成一个新的有序表。归并排序有多路归并排序、两路归并排序,
+ *              可用于内排序，也可以用于外排序。
+ * @author zch
+ * @time 2018年9月14日 下午6:30:18
+ * 
+ */
 public class MergeSortDemo extends SortBase {
 	public static void main(String[] args) {
-
-		int[] a = { 1, 3, 5 };
-		int[] b = { 2, 4, 6, 8, 10 };
-		int[] c = merge(a, b);
-		printArray(c);
+		System.out.println("排序之前");
+		printArray(a);
+		sort(a, 0, a.length - 1);
+		System.out.println("排序之后");
+		printArray(a);
 	}
 
-	static void sort(int[] a) {
-		while (true) {
+	static int[] newArray(int[] array, int start, int end) {
 
+		if (start > end) {
+			return array;
+		}
+		int[] ret = new int[end - start + 1];
+		int j = 0;
+		for (int i = start; i <= end; i++) {
+			ret[j] = array[i];
+			j++;
+		}
+		return ret;
+	}
+
+	static void sort(int[] array, int start, int end) {
+		System.out.println("\n单次分割");
+		printArray(array, start, end);
+
+		if (start >= end) {
+			return;
+		}
+
+		int mid = (start + end) / 2;
+		System.out.println("mid = " + mid);
+		System.out.println("分割结果");
+
+		printArray(array, start, mid);
+		sort(array, start, mid);
+
+		printArray(array, mid + 1, end);
+		sort(array, mid + 1, end);
+
+		mergeArray(array, start, mid, end);
+
+	}
+
+	static void mergeArray(int[] array, int start, int mid, int end) {
+
+		int[] array1 = newArray(array, start, mid);
+		int[] array2 = newArray(array, mid + 1, end);
+		int[] ret = merge(array1, array2);
+		for (int i = 0; i < ret.length; i++) {
+			array[start + i] = ret[i];
 		}
 	}
-	
-	static void mergeArray(int[] array, int start1, int start2, int start3) {
-		
-	}
-	
 
 	/**
 	 * 合并两个已经排序好的数组，需要一个额外的空间。
@@ -28,6 +70,9 @@ public class MergeSortDemo extends SortBase {
 	 * @return
 	 */
 	static int[] merge(int[] array1, int[] array2) {
+		System.out.println("\n单次合并");
+		printArray(array1);
+		printArray(array2);
 		int[] ret = new int[array1.length + array2.length];
 		int index1 = 0;
 		int index2 = 0;
@@ -58,6 +103,8 @@ public class MergeSortDemo extends SortBase {
 				break;
 			}
 		}
+		System.out.print("合并结果 ");
+		printArray(ret);
 		return ret;
 	}
 }
--- 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/SortBase.java
+++ 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/SortBase.java
@@ -2,11 +2,20 @@ package com.zch.blogs.algorithm.sort;
 
 public class SortBase {
 	static int a[] = { 57, 68, 59, 52, 72, 28, 96, 33, 24 };
-	
-	static void printArray(int[] a) {
-		for (int i : a) {
-			System.out.println(i);
-		}
 
+	static void printArray(int[] a) {
+
+		for (int i : a) {
+			System.out.print(i + " ");
+		}
+		System.out.println("");
+	}
+
+	static void printArray(int[] a, int start, int end) {
+
+		for (int i = start; i <= end; i++) {
+			System.out.print(a[i] + " ");
+		}
+		System.out.println("");
 	}
 }
