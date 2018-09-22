2cd21ce4dd1f71ebf04048c3bc310224e2d04f1f
wardensky <zhaochunhui1985@gmail.com>
2018-09-22 15:43:06 +0800

    a

--- 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/SortBase.java
+++ 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/SortBase.java
@@ -3,14 +3,14 @@ package com.zch.blogs.algorithm.sort;
 import java.util.Arrays;
 
 public class SortBase {
-	static int a[] = { 57, 68, 59, 52, 72, 28, 96, 33, 24 };
+	public static int a[] = { 57, 68, 59, 52, 72, 28, 96, 33, 24 };
 
-	static void printArray(int[] a) {
+	public static void printArray(int[] a) {
 		System.out.println(Arrays.toString(a));
-		 
+
 	}
 
-	static void printArray(int[] a, int start, int end) {
+	public static void printArray(int[] a, int start, int end) {
 
 		for (int i = start; i <= end; i++) {
 			System.out.print(a[i] + " ");
--- 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/topic/EvenOdd.java
+++ 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/topic/EvenOdd.java
@@ -1,6 +1,37 @@
 package com.zch.blogs.algorithm.topic;
 
-public class EvenOdd {
+import com.zch.blogs.algorithm.sort.SortBase;
 
-	
+public class EvenOdd extends SortBase {
+
+	public static void main(String[] args) {
+		printArray(a);
+		convert(a);
+		printArray(a);
+	}
+
+	static void convert(int[] array) {
+		int number = 0;
+		for (int i = 0; i < array.length; i++) {
+			boolean isEven = false;
+			if (array[i] % 2 == 0) {
+				number++;
+				isEven = true;
+			}
+			if (number > 0 && !isEven) {
+				System.out.println("number=" + number + " i=" + i);
+				swap(array, i, number);
+
+			}
+		}
+	}
+
+	static void swap(int[] array, int index, int number) {
+		for (int i = index; i > index - number; i--) {
+			int tmp = array[i];
+			array[i] = array[i - 1];
+			array[i - 1] = tmp;
+		}
+	}
+
 }
