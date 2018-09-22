6032ed6b86e3ccaf4fc546271f3484aa4486e9fa
wardensky <zhaochunhui1985@gmail.com>
2018-09-22 11:43:12 +0800

    a

--- 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/HeapSort.java
+++ 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/HeapSort.java
@@ -1,13 +1,12 @@
-package com.zch.blogs.algorithm.heap;
+package com.zch.blogs.algorithm.sort;
 
-import java.util.Arrays;
-
-public class HeapSort {
+public class HeapSort extends SortBase {
 
 	public static void main(String[] args) {
-		int[] arr = { 4,6,8,5,9 };
-		sort(arr);
-		System.out.println(Arrays.toString(arr));
+		//int[] arr = { 4, 6, 8, 5, 9 };
+		// sort(arr);
+		printArray(a);
+		sort(a);
 	}
 
 	public static void sort(int[] arr) {
@@ -15,13 +14,19 @@ public class HeapSort {
 		for (int i = arr.length / 2 - 1; i >= 0; i--) {
 			// 从第一个非叶子结点从下至上，从右至左调整结构
 			adjustHeap(arr, i, arr.length);
+			// myAdjustHeap(arr, i, arr.length);
+			printArray(arr);
 		}
+
+		System.out.println("_____");
 		// 2.调整堆结构+交换堆顶元素与末尾元素
 		for (int j = arr.length - 1; j > 0; j--) {
 			swap(arr, 0, j);// 将堆顶元素与末尾元素进行交换
+			System.out.println(arr[j] + " 放到队尾。");
 			adjustHeap(arr, 0, j);// 重新对堆进行调整
+			printArray(arr);
 		}
-
+		
 	}
 
 	/**
@@ -32,21 +37,41 @@ public class HeapSort {
 	 * @param length
 	 */
 	public static void adjustHeap(int[] arr, int i, int length) {
+		System.out.println("call adjustheap i = " + i + "    length = " + length);
 		int temp = arr[i];// 先取出当前元素i
-		for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {// 从i结点的左子结点开始，也就是2i+1处开始
+		for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {// 从i结点的左子结点开始，也就是2i+1处开始 在这个地方用k = k *2 +1很巧妙
 			if (k + 1 < length && arr[k] < arr[k + 1]) {// 如果左子结点小于右子结点，k指向右子结点
 				k++;
 			}
 			if (arr[k] > temp) {// 如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+				System.out.println("swap i=" + i + "  k=" + k);
 				arr[i] = arr[k];
 				i = k;
 			} else {
+				System.out.println("break");
 				break;
 			}
 		}
 		arr[i] = temp;// 将temp值放到最终的位置
 	}
 
+	public static void myAdjustHeap(int[] arr, int i, int length) {
+		int temp = arr[i];// 先取出当前元素i
+		int m = i;
+		for (int k = i * 2 + 1; k < length; k = m * 2 + 1) {// 从i结点的左子结点开始，也就是2i+1处开始
+			if (k + 1 < length && arr[k] < arr[k + 1]) {// 如果左子结点小于右子结点，k指向右子结点
+				k++;
+			}
+			if (arr[k] > temp) {// 如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
+				swap(arr, i, k);
+				i = k;
+				m = i;
+			} else {
+				break;
+			}
+		}
+
+	}
 
 	public static void swap(int[] arr, int a, int b) {
 		int temp = arr[a];
--- 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/SortBase.java
+++ 00.source-code/blogs/algorithm/src/main/java/com/zch/blogs/algorithm/sort/SortBase.java
@@ -1,14 +1,13 @@
 package com.zch.blogs.algorithm.sort;
 
+import java.util.Arrays;
+
 public class SortBase {
 	static int a[] = { 57, 68, 59, 52, 72, 28, 96, 33, 24 };
 
 	static void printArray(int[] a) {
-
-		for (int i : a) {
-			System.out.print(i + " ");
-		}
-		System.out.println("");
+		System.out.println(Arrays.toString(a));
+		 
 	}
 
 	static void printArray(int[] a, int start, int end) {
